7f11ab736b2c3276dd7a00e45904d31a31b8a4fc
Warren Qi <self.getmail+github@gmail.com>
2020-06-25 00:09:14 +0200

    woohoo prime relative chain by bfs working

--- ProjectEuler/src/main/java/euler/problems/Problem425.java
+++ ProjectEuler/src/main/java/euler/problems/Problem425.java
@@ -12,44 +12,110 @@ import euler.math.PrimeDbInMemory;
 import euler.math.PrimeSieve;
 import gnu.trove.list.TIntList;
 import gnu.trove.list.array.TIntArrayList;
+import gnu.trove.map.TIntIntMap;
+import gnu.trove.map.hash.TIntIntHashMap;
 import gnu.trove.set.TIntSet;
 import gnu.trove.set.hash.TIntHashSet;
 
 public class Problem425
 {
 
+    public static boolean detectLeft(String bigger, String smaller)
+    {
+        return bigger.substring(1)
+                     .equals(smaller);
+    }
+
+    /**
+     * True when either of these 2 conditions are met:
+     * <p>
+     * (1) A and B have the same length and differ in exactly one digit; for
+     * example, 123 ↔ 173.
+     * <p>
+     * (2) Adding one digit to the left of A (or B) makes B (or A); for example,
+     * 23 ↔ 223 and 123 ↔ 23. But NOT 2 and 23.
+     * 
+     */
+    public static boolean isConnected(int a, int b)
+    {
+        String aStr = String.valueOf(a);
+        String bStr = String.valueOf(b);
+        boolean leftDigit = false;
+        if (DigitUtils.numDigits(a) == DigitUtils.numDigits(b))
+        {
+            leftDigit = true;
+        } else if (DigitUtils.numDigits(a) > DigitUtils.numDigits(b))
+        {
+            leftDigit = detectLeft(aStr, bStr);
+        } else
+        {
+            leftDigit = detectLeft(bStr, aStr);
+        }
+
+        // requring edit distance == 1 also dedups the input x, which is helpful
+        // to avoid loopbacks
+        return leftDigit && 1 == StringUtils.getLevenshteinDistance(aStr, bStr);
+    }
+
     public static void main(String[] args)
     {
         Problem425 t = new Problem425(10_000);
-        System.out.println("2 -> " + t.searchStep(2, 127));
-        System.out.println("3 -> " + t.searchStep(3, 127));
-        System.out.println("13 -> " + t.searchStep(13, 127));
-        System.out.println("113 -> " + t.searchStep(113, 127));
-        System.out.println("103 -> " + t.searchStep(103, 127));
+        System.out.println("2 -> " + t.searchStep(2, 100));
+        System.out.println("3 -> " + t.searchStep(3, 100));
+        System.out.println("5 -> " + t.searchStep(5, 100));
+        System.out.println("7 -> " + t.searchStep(7, 100));
+        // System.out.println("13 -> " + t.searchStep(13, 127));
+        // System.out.println("113 -> " + t.searchStep(113, 127));
+        // System.out.println("103 -> " + t.searchStep(103, 127));
+        // System.out.println("107 -> " + t.searchStep(107, 127));
+
+        // now find all prime relatives of 2. to start, limit to 1_000
+        t = new Problem425(1_000);
+        TIntList primeRelatives = t.primeRelativesChain(2, 97);
+        System.out.println("primeRelatives of 2 -> 97 = " + primeRelatives);
+
+        t = new Problem425(1_000);
+        primeRelatives = t.primeRelativesChain(2, 127);
+        System.out.println("primeRelatives of 2 -> 127 = " + primeRelatives);
+
+        t = new Problem425(1_000);
+        primeRelatives = t.primeRelativesChain(2, 103);
+        System.out.println("primeRelatives of 2 -> 103 = " + primeRelatives);
+
+        // cache the intermediate chains
+
+        // then eliminate all prime relatives of 2, sum them up to output
 
     }
 
     private final PrimeSieve primes;
+
+    private final Map<Integer, TIntList> cache = new HashMap<>();
+
+    private final TIntSet visited = new TIntHashSet();
 
     public Problem425(int maxPrime)
     {
         primes = new PrimeSieve(new PrimeDbInMemory(), maxPrime);
     }
 
-    public Pair<Integer, Integer> getRange(int x)
+    /**
+     * A single search step towards finding a chain of connected primes. This
+     * method avoids loopbacks.
+     * 
+     * @param x
+     * @param maxP
+     * @return A list of connected primes less than or equal to maxP, excluding
+     *         the "visited" primes of this instance. Empty if none found.
+     */
+    public TIntList searchStep(int x, int maxP)
     {
-        int numDigits = DigitUtils.numDigits(x);
-        // if x in range [0,9], min = 2; max = 99
-        // if x in range [10,99], e.g. 17, min = 7; max = 917 (or just 999)
-        int min = 2;
-        min = Math.max(min, (int) Math.pow(10, numDigits - 2));
-
-        int max = Integer.MAX_VALUE;
-        max = Math.min(max, (int) Math.pow(10, numDigits + 1) - 1);
-        return new ImmutablePair<Integer, Integer>(min, max);
+        TIntList connectedPrimes = connectedPrimes(x, maxP);
+        visited.add(x);
+        TIntList res = filterVisited(connectedPrimes, visited);
+        visited.addAll(connectedPrimes);
+        return res;
     }
-
-    private final Map<Integer, TIntList> cache = new HashMap<>();
 
     /**
      * 
@@ -99,51 +165,91 @@ public class Problem425
         return res;
     }
 
-    private final TIntSet visited = new TIntHashSet();
-
-    public TIntList searchStep(int x, int maxP)
+    public Pair<Integer, Integer> getRange(int x)
     {
-        TIntList connectedPrimes = connectedPrimes(x, maxP);
-        visited.add(x);
-        TIntList res = filterVisited(connectedPrimes, visited);
-        return res;
+        int numDigits = DigitUtils.numDigits(x);
+        // if x in range [0,9], min = 2; max = 99
+        // if x in range [10,99], e.g. 17, min = 7; max = 917 (or just 999)
+        int min = 2;
+        min = Math.max(min, (int) Math.pow(10, numDigits - 2));
+
+        int max = Integer.MAX_VALUE;
+        max = Math.min(max, (int) Math.pow(10, numDigits + 1) - 1);
+        return new ImmutablePair<Integer, Integer>(min, max);
     }
 
     /**
-     * True when either of these 2 conditions are met:
-     * <p>
-     * (1) A and B have the same length and differ in exactly one digit; for
-     * example, 123 ↔ 173.
-     * <p>
-     * (2) Adding one digit to the left of A (or B) makes B (or A); for example,
-     * 23 ↔ 223 and 123 ↔ 23. But NOT 2 and 23.
+     * 
+     * @param x
+     * @param target
+     * @return The shortest chain (by BFS) of primes between X and Target, each
+     *         pair "connected", with no prime in this chain larger than Target.
+     *         Empty list if no such chain of primes exists between X and
+     *         Target.
      * 
      */
-    public static boolean isConnected(int a, int b)
+    public TIntList primeRelativesChain(int x, int target)
     {
-        String aStr = String.valueOf(a);
-        String bStr = String.valueOf(b);
-        boolean leftDigit = false;
-        if (DigitUtils.numDigits(a) == DigitUtils.numDigits(b))
+        // Use a node -> parent mapping to back trace our steps
+        TIntIntMap leafToRoots = new TIntIntHashMap();
+
+        TIntList queue = new TIntArrayList();
+        queue.add(x);
+        boolean found = false;
+        while (queue.size() > 0)
         {
-            leftDigit = true;
-        } else if (DigitUtils.numDigits(a) > DigitUtils.numDigits(b))
+            int val = queue.removeAt(0);
+
+            TIntList valConnectedPrimes = searchStep(val, target);
+
+            if (!valConnectedPrimes.isEmpty())
+            {
+
+                for (int i = 0; i < valConnectedPrimes.size(); i++)
+                {
+                    int connecteePrime = valConnectedPrimes.get(i);
+                    leafToRoots.put(connecteePrime, val);
+                }
+                if (valConnectedPrimes.contains(target))
+                {
+                    found = true;
+                    break;
+                } else
+                {
+                    queue.addAll(valConnectedPrimes);
+                }
+            }
+
+        }
+        TIntList res = new TIntArrayList();
+
+        if (found)
         {
-            leftDigit = detectLeft(aStr, bStr);
-        } else
-        {
-            leftDigit = detectLeft(bStr, aStr);
+            int tracer = target;
+            while (leafToRoots.containsKey(tracer))
+            {
+                res.add(tracer);
+                // at every step, find parent of current step
+                tracer = leafToRoots.get(tracer);
+            }
+            res.add(x);
+            res.reverse();
         }
 
-        // requring edit distance == 1 also dedups the input x, which is helpful
-        // to avoid loopbacks
-        return leftDigit && 1 == StringUtils.getLevenshteinDistance(aStr, bStr);
+        return res;
     }
 
-    public static boolean detectLeft(String bigger, String smaller)
+    public TIntSet primeSetDiff(int startingPrime, TIntSet relatives, int maxP)
     {
-        return bigger.substring(1)
-                     .equals(smaller);
+        TIntSet res = new TIntHashSet();
+        for (int i = startingPrime; i < maxP; i++)
+        {
+            if (primes.isPrime(i) && !relatives.contains(i))
+            {
+                res.add(i);
+            }
+        }
+        return res;
     }
 
 }
